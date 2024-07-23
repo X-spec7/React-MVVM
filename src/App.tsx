67c54a3afe7f975b728442fbf68957e44b1f6799
X-spec7 <mercury.spec77@gmail.com>
2024-07-24 00:41:15 +0900

    feat(ui): build ui structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./Presentation/Views/Product/List/ProductList";
+import ProductDetail from "./Presentation/Views/Product/Detail/ProductDetail";
+import CreateProduct from "./Presentation/Views/Product/New/CreateProduct";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/new" element={<CreateProduct />} />
+          <Route path="/product/detail/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
